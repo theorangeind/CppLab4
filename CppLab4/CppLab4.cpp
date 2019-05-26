3c2e04ecc6f4783dd7800885fd6323e8c3df62de
TheOrangeInd <TheOrangeInd@DESKTOP-AAO5NUL>
2019-05-26 11:49:39 +0300

    Добавьте файлы проекта.

---
+++ CppLab4/CppLab4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	string result;
+
+	cout << "Enter a sentence:\n";
+	getline(cin, s);
+
+	stringstream ss;
+	ss << s;
+
+	for (string word; ss >> word;)
+	{
+		reverse(word.begin(), word.end());
+		result += word + " ";
+	}
+	reverse(result.begin(), result.end());
+	result = result.substr(1, s.length());
+
+	cout << "\nReversed:\n";
+
+	cout << result << "\n";
+
+	system("pause");
+	return 0;
+}
